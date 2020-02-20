1dfffc16f6bc7c2d35b8f7588e92694339b54b7f
Jonathan Weaver <jweaver5@usf.edu>
2020-02-19 19:08:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Iterative_Statements
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter an integer between 2 and 10: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                for (int i = 0; i < num + 1; i++)
+                    Console.WriteLine("{0}. You entered an even number.", i + 1);
+            }
+            else
+            {
+                for (int i = 0; i < num - 1; i++)
+                    Console.WriteLine("{0}. You entered an odd number.", i + 1);
+            }
+        }
+    }
+}
